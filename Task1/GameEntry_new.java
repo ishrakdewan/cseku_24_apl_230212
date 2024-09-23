9a16ad12a2e7c7747bbd9b3acbebd702a2c45bb6
Muhammad Ishrak Dewan <166523403+ishrakdewan@users.noreply.github.com>
2024-09-23 08:13:09 +0600

    GameEntry_new.java

--- Task1/GameEntry_new.java
+++ Task1/GameEntry_new.java
@@ -1,33 +1,39 @@
 package GameNew;
 
-public class GameEntry_new {
+/**
+ * The GameEntry class represents a single entry in a game leaderboard.
+ * It stores the player's name and their score.
+ */
+public class GameEntry {
     private String playerName;
     private int score;
 
-    public GameEntry_new(String name, int score) {
+      
+     
+    public GameEntry(String name, int score) {        //Constructs a new GameEntry with the specified player's name and score.
         this.playerName = name;
         this.score = score;
     }
 
-    public void setName(String name) {
+    public void setName(String name) {            // Sets the player's name.
         this.playerName = name;
     }
 
-    public void setScore(int score) {
+    public void setScore(int score) {           // Sets the player's score.
         this.score = score;
     }
 
-    public String getName() {
+    public String getName() {          //Returns the player's name.
         return this.playerName;
     }
 
-    public int getScore() {
+    public int getScore() {         // Returns the player's score.
         return this.score;
     }
-
+    
     @Override
-    public String toString() {
+    public String toString() {    // Returns a string representation of the game entry.
+
         return "(" + this.playerName + ", " + this.score + ")";
     }
 }
-
